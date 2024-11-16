a35998ea8bbeece62d64822e3fcc1039b8b73e7d
Jack Dewinter <jackdewinter1@gmail.com>
2024-11-16 23:13:11 +0000

    update repo ui

--- apps/nextjs/src/app/_components/repo.tsx
+++ apps/nextjs/src/app/_components/repo.tsx
@@ -4,13 +4,14 @@ import { useState } from "react";
 import { useDevlabRuns } from "@/hooks/use-devlab-runs";
 import { Button } from "@devlab/ui/button";
 import { Input } from "@devlab/ui/input";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import { api } from "../../trpc/react";
 
-export function LatestRepo() {
+export function Repo() {
   const { repoRuns } = useDevlabRuns();
-
-  const [repos] = api.repo.getAll.useSuspenseQuery();
 
   const utils = api.useUtils();
   const [name, setName] = useState("");
@@ -24,15 +25,7 @@ export function LatestRepo() {
   });
 
   return (
-    <div className="w-full max-w-xs">
-      {repos.length > 0 ? (
-        <p className="truncate">
-          Your most recent repos:{" "}
-          {repos.map((repo) => `${repo.owner}/${repo.name}`).join(", ")}
-        </p>
-      ) : (
-        <p>You have no repos yet.</p>
-      )}
+    <div className="flex w-full flex-col gap-4">
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -58,7 +51,43 @@ export function LatestRepo() {
       </form>
       <div>
         {repoRuns.map((run) => (
-          <div key={run.id}>Readme: {run.output?.readme}</div>
+          <div key={run.id}>
+            {run.output?.readme ? (
+              <ReactMarkdown
+                children={run.output.readme}
+                components={{
+                  h1: ({ children }) => {
+                    return <h1 className="text-2xl font-bold">{children}</h1>;
+                  },
+                  code({ inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className ?? "");
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, "")}
+                        // @ts-expect-error dark is a valid style
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                        style={dark}
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                      />
+                    ) : (
+                      <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, "")}
+                        // @ts-expect-error dark is a valid style
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                        style={dark}
+                        PreTag="div"
+                        {...props}
+                      />
+                    );
+                  },
+                }}
+              />
+            ) : (
+              <p className="text-gray-500">No readme found</p>
+            )}
+          </div>
         ))}
       </div>
     </div>
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,29 +1,17 @@
-import { api, HydrateClient } from "../trpc/server";
-import { LatestRepo } from "./_components/repo";
+import { HydrateClient } from "../trpc/server";
+import { Repo } from "./_components/repo";
 import { TriggerExample } from "./_components/trigger-example";
 
-export default async function Home() {
-  const repos = await api.repo.getAll();
-
-  void api.repo.getAll.prefetch();
-
+export default function Home() {
   return (
     <HydrateClient>
-      <main className="dark:bg-secondaryBlack inset-0 flex min-h-[100dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="inset-0 flex min-h-[100dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] dark:bg-secondaryBlack">
+        <div className="container flex max-w-screen-sm flex-col items-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Dev <span className="text-[hsl(280,100%,70%)]">Lab</span>
           </h1>
 
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {repos.length > 0
-                ? repos.map((repo) => `${repo.owner}/${repo.name}`).join(", ")
-                : "No repos found"}
-            </p>
-          </div>
-
-          <LatestRepo />
+          <Repo />
           <TriggerExample />
         </div>
       </main>
